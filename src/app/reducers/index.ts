03acb260b7fcb5e67d3fc765f9fcb457054aa63a
John Christopher Jones <john.christopher@alumni.virginia.edu>
2018-12-06 00:49:06 -0500

    chore: add hotels and locations to app state
    The Entity schematic didn't actually add the entities to our App State, so we
    need to do that now.

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -6,14 +6,22 @@ import {
   MetaReducer
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
+import * as fromHotels from './hotel.reducer';
+import * as fromLocations from './location.reducer';
 
 export interface State {
-
+  hotels: fromHotels.State;
+  locations: fromLocations.State;
 }
 
 export const reducers: ActionReducerMap<State> = {
-
+  hotels: fromHotels.reducer,
+  locations: fromLocations.reducer
 };
 
 
 export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
+
+export const selectHotels = createFeatureSelector<fromHotels.State>('hotels');
+
+export const selectLocations = createFeatureSelector<fromLocations.State>('locations');
