df78bcb5f14f962d6f8de266eafb20537153b8f6
John Christopher Jones <john.christopher@alumni.virginia.edu>
2019-04-11 15:53:04 -0400

    style: rework grid style to be responsive and closer to spec
    Make the main area grid layout more responive and mobile-first.
    
    TODO: The app bar still needs to be mobile-friendly.
    TODO: It would improve SEO to lay out the main app area with semantic markup.

--- src/app/components/hotel-grid-cell/hotel-grid-cell.component.ts
+++ src/app/components/hotel-grid-cell/hotel-grid-cell.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Hotel } from 'src/app/models/hotel.model';
+import { hotelsUri } from 'src/app/services/roomkey-api.service';
 
 @Component({
   selector: 'app-hotel-grid-cell',
@@ -8,6 +9,8 @@ import { Hotel } from 'src/app/models/hotel.model';
 })
 export class HotelGridCellComponent implements OnInit {
   @Input() hotel: Hotel;
+  showMore = false;
+  MAX_AMENITIES = 6;
 
   constructor() { }
 
@@ -19,4 +22,15 @@ export class HotelGridCellComponent implements OnInit {
     return `https://d29u3c1wxehloe.cloudfront.net${primaryPhoto.id}${size}.jpg`;
   }
 
+  amenitiesList() {
+    return this.showMore ? this.hotel.amenities : this.hotel.amenities.slice(0, this.MAX_AMENITIES);
+  }
+
+  showMoreButton() {
+    return this.hotel.amenities.length > this.MAX_AMENITIES;
+  }
+
+  showMoreButtonLabel() {
+    return this.showMore ? 'SHOW FEWER AMENITIES' : 'SHOW MORE AMENITIES';
+  }
 }
